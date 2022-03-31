be3d89632cf55c7fbb1fac1049dda7d1444f522e
rosario <andolinarosario@gmail.com>
2022-03-31 23:38:43 +0200

    dataloaders model criterion optimizer

--- src/main.py
+++ src/main.py
@@ -8,32 +8,45 @@ from .model import MPL
 from .data import DataProcessing, DataTransform
 from os import getenv
 
+
+# Training settings
+parser = argparse.ArgumentParser('psychxrf')
+parser.add_argument('--h5datafile', type = str, required = True, help = 'HDF5 data file')
+parser.add_argument('--batch-size', type = int, default = 64, help = 'training batch size [64]')
+parser.add_argument('--test-batch-size', type = int, default = 32, help = 'testing batch size [32]')
+parser.add_argument('--num-epoch', type = int, default = 800, help = 'number of epoch to train for [800]')
+parser.add_argument('--lr', type = float, default = 0.01, help = 'learning rate [0.01]')
+parser.add_argument('--hidden-sizes', nargs = '+', default = [128], help = 'sequence of hidden layer sizes')
+parser.add_argument('--optimizer', type = str, default = 'sgd', help = 'Optimizer. One of "sgd" "adam" [sgd]')
+parser.add_argument('--trans-file', type = str, default = f'/home/{getenv("USER")}/test_transform_file.h5', help = 'HDF5 file were inputs & targets transformation parameters are stored')
+
+opt = parser.parse_args()
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+print("###### LOADING DATASETS ######")
+dproc = DataProcessing().load_h5_data(opt.h5datafile)
+dtrans = DataTransform(dproc.get_inputs_from_labels(), dproc.get_targets(), transform_file = opt.trans_file)
+dtrans.input_transform()
+train_set = dtrans.get_training_set()
+test_set = dtrans.get_testing_set()
+training_dataloader = DataLoader(train_set, opt.batch_size, shuffle = True)
+testing_dataloader = DataLoader(test_set, opt.test_batch_size, shuffle = True)
+
+print("###### Building Model ######")
+model = MPL(in_size = dtrans.inputs.shape[1], out_size = dtrans.targets.shape[1], hidden_sizes = opt.hidden_sizes)
+criterion = nn.MSELoss()
+
+if opt.optimizer == 'sgd':
+    optimizer = optim.SGD(model.parameters(), lr = opt.lr)
+elif opt.optimizer == 'adam':
+    optimizer = optim.Adam(model.parameters(), lr = opt.lr)
+else:
+    raise ValueError('Unutilized optimizer')
+print(model)
+
 def main():
-    # Training settings
-    parser = argparse.ArgumentParser('psychxrf')
-    parser.add_argument('--h5datafile', type = str, required = True, help = 'HDF5 data file')
-    parser.add_argument('--batch-size', type = int, default = 64, help = 'training batch size [64]')
-    parser.add_argument('--test-batch-size', type = int, default = 32, help = 'testing batch size [32]')
-    parser.add_argument('--num-epoch', type = int, default = 800, help = 'number of epoch to train for [800]')
-    parser.add_argument('--lr', type = float, default = 0.01, help = 'learning rate [0.01]')
-    parser.add_argument('--hidden-sizes', nargs = '+', default = [128], help = 'sequence of hidden layer sizes')
-    parser.add_argument('--trans-file', type = str, default = f'/home/{getenv("USER")}/test_transform_file.h5', help = 'HDF5 file were inputs & targets transformation parameters are stored')
-
-    opt = parser.parse_args()
-    
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-    print("###### LOADING DATASETS ######")
-    dproc = DataProcessing().load_h5_data(opt.h5datafile)
-    dtrans = DataTransform(dproc.get_inputs_from_labels(), dproc.get_targets(), transform_file = opt.trans_file)
-    dtrans.input_transform()
-    train_set = dtrans.get_training_set()
-    test_set = dtrans.get_testing_set()
-    training_dataloader = DataLoader(train_set, opt.batch_size, shuffle = True)
-    testing_dataloader = DataLoader(test_set, opt.test_bacth_size, shuffle = True)
-    # to be continued
-    
-
+    print("training...")
 
 if __name__ == '__main__':
     main()
--- src/model.py
+++ src/model.py
@@ -19,3 +19,13 @@ class MPL(nn.Module):
         x = self.network(x)
         x = F.relu(x)
         return x
+
+
+#Root Means Squared Log Error
+class RMSLELoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.mse = nn.MSELoss()
+    
+    def forward(self, pred, actual):
+        return torch.sqrt(self.mse(torch.log(pred + 1), torch.log(actual + 1)))
