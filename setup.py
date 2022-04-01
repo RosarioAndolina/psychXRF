11f98b3dbbbf49bdb573c66b1037f80bd5af32ea
rosario <andolinarosario@gmail.com>
2022-04-01 20:55:21 +0200

    new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='psychXRF',
-    version='0.3',
+    version='0.4',
     description='Predict synthetic multichannel XRF spectra simulation parameters',
     author='Rosario Andolina',
     author_email='andolinarosario@gmail.com',
